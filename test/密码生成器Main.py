c5cae366e573e1b08ec78882b0ac31e3fd1a7f54
wuzubei <wuzubei1986@icloud.com>
2024-09-23 00:23:45 +0800

    test

---
+++ test/密码生成器Main.py
@@ -0,0 +1,32 @@
+import string
+import random
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+
+from 密码生成器 import Ui_PasswordGenerate
+import sys
+
+class MyPwdSC(Ui_PasswordGenerate, QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+        self.btn_newPwd.clicked.connect(self.new_pwd)
+
+    def new_pwd(self):
+        words = (
+            string.digits
+            + string.ascii_uppercase
+            + string.ascii_lowercase
+            + string.punctuation
+        )
+        words = random.sample(list(words),20)
+        pwd="".join(words)
+        self.lineEdit.setText(pwd)
+        QMessageBox.information(self,'信息提示','密码生成成功')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myPwdSC = MyPwdSC()
+    sys.exit(app.exec())
---
+++ test/第一个pyqt程序.py
@@ -0,0 +1,27 @@
+import sys
+
+from PyQt6.QtWidgets import (
+    QApplication, QDialog, QPushButton, QHBoxLayout, QMessageBox
+)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = QDialog()
+    window.setWindowTitle('Tip')
+    window.resize(400,300)
+    def show_msg():
+        QMessageBox.information(window, '信息提示', '你点击了我')
+#布局
+    hbox = QHBoxLayout()
+#按钮
+    button= QPushButton('点击我')
+    button.clicked.connect(show_msg)
+#布局里添加按钮
+    hbox.addWidget(button)
+#窗口中添加布局组件
+    window.setLayout(hbox)
+#显示 窗口
+    window.show()
+#窗口停止事件
+    sys.exit(app.exec())
+
